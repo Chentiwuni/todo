03b7e5104b8aaca1736f97cc6616ada20a181554
Chentiwuni <chienntiwuni@gmail.com>
2025-04-06 22:49:28 +0000

    added firebase packages

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Only declare the google-services plugin here
+        classpath("com.google.gms:google-services:4.4.2")
+        // Add other classpaths here if needed
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -12,6 +24,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
